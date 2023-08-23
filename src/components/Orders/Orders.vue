0e2a936b63c817e204202a2abad0d3f848a963ee
Filip <filipsliskovic00@gmail.com>
2023-08-23 21:09:27 +0200

    continued working on my orders

--- src/components/Orders/Orders.vue
+++ src/components/Orders/Orders.vue
@@ -39,9 +39,14 @@
                   :items=this.Tables
                   item-value="id"
                   item-title="name"
+                  v-model="SelectedTable"
                   
                 ></v-select>
                 <v-card-actions class="justify-end">
+                  <v-btn
+                    variant="text"
+                    @click="createNewOrder(this.SelectedTable); isActive.value = false"
+                  >Create</v-btn>
                   <v-btn
                     variant="text"
                     @click="isActive.value = false"
@@ -213,6 +218,7 @@
         Orders : [],
         Tables : [],
         headers: [],
+        SelectedTable: 0,
       }),
   
       computed: {
@@ -292,11 +298,50 @@
             });
           }
 
-          
-        }
-
-        ,
+        },
         
+        createNewOrder(tableId)
+        {
+
+          if(tableId && tableId > 0)
+          {
+            var Order = {
+              tableId: tableId,
+              DateAndTime: new Date()
+            }
+            axios
+            .post("http://localhost:5000/api/orders",Order,{
+                headers: { Authorization: "Bearer " + this.$store.getters.Token },
+              })
+            .then(response=>{
+              console.log(response)
+              // this.dataItems.push(this.editedItem)
+              alert("Created!")
+            })
+            .catch(error => {
+              if (error.response) {
+                // The request was made and the server responded with a status code
+                // that falls out of the range of 2xx
+                console.log(error.response.data);
+                console.log(error.response.status);
+                console.log(error.response.headers);
+              } else if (error.request) {
+                // The request was made but no response was received
+                // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+                // http.ClientRequest in node.js
+                console.log(error.request);
+              } else {
+                // Something happened in setting up the request that triggered an Error
+                console.log('Error', error.message);
+              }
+              console.log(error.config);
+            })
+          }
+
+          
+
+
+        },
   
         editItem (item) {
           this.editedIndex = this.desserts.indexOf(item)
